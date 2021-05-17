1cdfbf4ecfacb78264fcfd81911a8d9a1af48bdc
EErasmus <eraum.erasmus@gmail.com>
2021-05-17 16:05:36 +0200

    Full Commit

---
+++ app/src/main/java/com/example/inventorymanagementapp/EditWishlist.java
@@ -0,0 +1,14 @@
+package com.example.inventorymanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditWishlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_wishlist);
+    }
+}
