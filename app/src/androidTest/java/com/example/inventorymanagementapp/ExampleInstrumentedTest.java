bc76c536d50678e89cae9ef483fefca1f8c973b1
yash <yashvardhan.bhardwaj@outlook.com>
2021-06-09 12:08:07 +0200

    sample unit test checking what is typed

--- app/src/androidTest/java/com/example/inventorymanagementapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/inventorymanagementapp/ExampleInstrumentedTest.java
@@ -2,13 +2,26 @@ package com.example.inventorymanagementapp;
 
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +30,28 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<LoginActivity> activityRule
+            = new ActivityScenarioRule<>(LoginActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.inventorymanagementapp", appContext.getPackageName());
     }
+    @Test
+    public void changeText_sameActivity() {
+        String stringToBetyped = "899";
+
+        // Type text and then press the button.
+        onView(withId(R.id.editTextTextPersonName))
+                .perform(typeText(stringToBetyped));
+        // onView(withId(R.id.loginBtnMainCont)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.editTextTextPersonName))
+                .check(matches(withText(stringToBetyped)));
+    }
+
 }
